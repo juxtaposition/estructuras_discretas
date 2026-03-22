bd7a4a6c7b8393f112485828baf892ed5ebb4e95
Gerardo Ezequiel Duran Perez <exbios@ciencias.unam.mx>
2026-03-21 23:23:01 -0600

    chroe: linter code

--- EstructurasDiscretas/Practica4/Practica4.hs
+++ EstructurasDiscretas/Practica4/Practica4.hs
@@ -5,11 +5,15 @@ import Mati (Matrioska(..))
 -- Este metodo lo que hace tomar un entero y devolver su similar con un tipo de dato DTNat 
 a_natural :: Int -> DTNat 
 a_natural 0 = Cero
+
+-- Recursion
 a_natural x = S ( a_natural (x-1))
 
 -- Este metodo toma un tipo de Dato DTNat (S (S Cero)) y regresa un entero
 a_entero :: DTNat -> Int
 a_entero Cero = 0
+
+-- Recursion
 a_entero (S x) = 1 + (a_entero x)
 
 -- Este metodo toma dos DTNat y regresa la potencia de un DTNat elevado a un DTNat
@@ -18,13 +22,15 @@ potenciaNat Cero Cero = (S Cero)
 potenciaNat (S n) Cero = (S Cero)
 potenciaNat Cero (S m) = Cero
 potenciaNat (S n) (S Cero) = (S n)
-
+ 
+-- Recursion
 potenciaNat (S n) (S m) =  multiNat (S n) (potenciaNat (S n) m) 
 
 -- Este metodo toma un DNat y regres su factorial
 facNat :: DTNat -> DTNat
 facNat Cero = (S Cero)
 
+-- Recursion
 facNat (S n) = multiNat (S n) (facNat n)
 
 -- Metodo que multiplica dos Ent (enteros) y regresa un Ent
@@ -35,6 +41,7 @@ multiEnt Zero (Succ Zero) = Zero
 multiEnt (Succ Zero) (Succ m) = (Succ m)
 multiEnt (Succ n) (Succ Zero) = (Succ n)
 
+-- Recursion
 multiEnt (Succ n) (Succ m) =  suma_ent (Succ n) (multiEnt (Succ n) m)
 
 -- Funcion mayor or igual que compra dos matrioskas
@@ -42,9 +49,13 @@ mayorIgual Mati Mati = True
 mayorIgual (Cont Mati) (Mati) = True
 mayorIgual Mati (Cont m) = False
 
+-- Recursion
 mayorIgual (Cont n) (Cont m) = mayorIgual n m
 
 
 -- Funcion aplana que convierte una lista planta de todas las matrioskas que la componen
 aplana Mati = [Mati]
+
+-- Recursion
 aplana (Cont n) = [(Cont n)] ++ aplana(n)
+
--- EstructurasDiscretas/Practica4/natural.hs
+++ EstructurasDiscretas/Practica4/natural.hs
@@ -7,6 +7,8 @@ sumaNat :: DTNat -> DTNat -> DTNat
 sumaNat (Cero) (Cero) = Cero
 sumaNat (Cero) (S m) = S ( sumaNat Cero m)
 sumaNat (S n) (Cero) = S ( sumaNat n Cero)
+
+-- Recursion
 sumaNat (S n) (S m) = S (S (sumaNat n m))
 
 -- Esta funcion hace uso de la sumaNat para poder multiplicar dos DTNat
@@ -16,6 +18,8 @@ multiNat (S Cero) Cero = Cero
 multiNat Cero (S Cero) = Cero
 multiNat (S Cero) (S m) = (S m)
 multiNat (S n) (S Cero) = (S n)
+
+-- Recursion
 multiNat (S n) (S m) =  sumaNat (S n) (multiNat (S n) m)
 
 
