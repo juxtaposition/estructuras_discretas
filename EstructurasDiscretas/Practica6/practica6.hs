c4cf52262470bbf705d1e57a64780160fe18fd8d
Gerardo Ezequiel Duran Perez <exbios@ciencias.unam.mx>
2026-04-20 16:13:23 -0600

    function nVacios created

--- EstructurasDiscretas/Practica6/practica6.hs
+++ EstructurasDiscretas/Practica6/practica6.hs
@@ -1,2 +1,11 @@
 module Practica5 where
 
+data Arbol a = Vacio | AB a (Arbol a) (Arbol a) deriving (Eq, Ord, Show)
+
+nVacios :: Arbol a -> Int
+nVacios Vacio = 0
+nVacios (AB n Vacio Vacio) = 2
+nVacios (AB n t1 Vacio) = 1 + nVacios(t1)
+nVacios (AB n Vacio t2) = 1 + nVacios(t2)
+nVacios (AB n t1 t2) = nVacios(t1) + nVacios(t2)
+
