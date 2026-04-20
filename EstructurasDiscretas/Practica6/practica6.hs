44d11698f17ca6b1f0734f978d4f92bb4f938650
Gerardo Ezequiel Duran Perez <exbios@ciencias.unam.mx>
2026-04-20 16:34:18 -0600

    added reflaja function

--- EstructurasDiscretas/Practica6/practica6.hs
+++ EstructurasDiscretas/Practica6/practica6.hs
@@ -10,4 +10,9 @@ nVacios (AB n Vacio t2) = 1 + nVacios(t2)
 nVacios (AB n t1 t2) = nVacios(t1) + nVacios(t2)
 
 
-
+refleja :: Arbol a -> Arbol a
+refleja Vacio = Vacio
+refleja (AB n Vacio Vacio) = AB n Vacio Vacio
+refleja (AB n t1 Vacio) = AB n Vacio (refleja t1)
+refleja (AB n Vacio t2) = AB n (refleja t2) Vacio
+refleja (AB n t1 t2) = AB n (refleja t2) (refleja t1)
